bdf755590b276a48320263b77bfcb6a3ebf6763c
Gustavo Casanova <gustavo.casanova@nicebots.com>
2018-09-28 07:44:45 -0300

    WIP

--- timonel-bootloader/tml-bootloader.c
+++ timonel-bootloader/tml-bootloader.c
@@ -168,6 +168,7 @@ int main() {
                         // If no, it means that the application is too big, erase the application
                         // TODO: Implement ALLOW_USE_TPL_PG (allow use trampoline page)
                         // TODO: Implement AUTO_TPL_CALC (auto-trampoline calculation & flash)
+                        // TODO: Implement a GETTMNLV reply code that indicates the commands available
                         
                         // //tmpPage[PAGE_SIZE] = { 0xFF };
                         // const __flash unsigned char * flashAddr;
