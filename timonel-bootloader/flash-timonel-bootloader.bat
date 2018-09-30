7c665a18510d27411c1ddf1c08da40aa84b9df72
Gustavo Casanova <gustavo.casanova@nicebots.com>
2018-09-30 03:24:02 -0300

    Update flash-timonel-bootloader.bat

--- timonel-bootloader/flash-timonel-bootloader.bat
+++ timonel-bootloader/flash-timonel-bootloader.bat
@@ -7,6 +7,4 @@
 :echo "***********************************************************************"
 :echo ""
 
-:del rudder-interrupt-free.* & make all & avr-size rudder-interrupt-free.elf & avr-size rudder-interrupt-free.hex & avrdude -c USBasp -p attiny85 -U flash:w:rudder-interrupt-free.hex:i -B 20
-
 avrdude -c USBasp -p attiny85 -U flash:w:.\\releases\\tml-bootloader.hex:i -B 20
