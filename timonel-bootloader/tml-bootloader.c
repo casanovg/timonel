652787f0385716f310fa17a6db75a5de63a21218
Gustavo Casanova <gustavo.casanova@nicebots.com>
2018-10-03 18:57:41 -0300

    Fix temporary page buffer erasing before flashing a new app

--- timonel-bootloader/tml-bootloader.c
+++ timonel-bootloader/tml-bootloader.c
@@ -85,6 +85,8 @@ int main() {
     Usi_onReceiverPtr = ReceiveEvent;       /* I2C Receive Event */
     Usi_onRequestPtr = RequestEvent;        /* I2C Request Event */
     statusRegister = (1 << ST_APP_READY);   /* In principle, assume that there is a valid app in memory */
+    __SPM_REG = (_BV(CTPB) | _BV(__SPM_ENABLE)); /* Clear temporary page buffer */
+    asm volatile("spm");    
     word dlyCounter = TOGGLETIME;
     byte exitDly = CYCLESTOEXIT;            /* Delay to exit bootloader and run the application if not initialized */
     /*  ___________________
