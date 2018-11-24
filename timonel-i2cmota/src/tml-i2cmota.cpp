f001fa45c6ac3abc3e448af2668d0e520b7d1a1a
Gustavo Casanova <gustavo.casanova@nicebots.com>
2018-11-24 02:48:14 -0300

    WIP

--- timonel-i2cmota/src/tml-i2cmota.cpp
+++ timonel-i2cmota/src/tml-i2cmota.cpp
@@ -1,9 +1,80 @@
 #include <Arduino.h>
 
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266httpUpdate.h>
+
+#define USE_SERIAL Serial
+
+ESP8266WiFiMulti WiFiMulti;
+
+// Blink
+const int ledPin =  2;                // the number of the LED pin (2 = GPIO2 / ESP-01)
+int ledState = LOW;                   // ledState used to set the LED
+unsigned long previousMillis = 0;     // will store last time LED was updated
+const long interval = 500;            // interval at which to blink (milliseconds)
+
 void setup() {
   // put your setup code here, to run once:
+  USE_SERIAL.begin(9600);
+  USE_SERIAL.setDebugOutput(true);
+
+  USE_SERIAL.println();
+  USE_SERIAL.println();
+  USE_SERIAL.println();
+
+  for (uint8_t t = 4; t > 0; t--) {
+    USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
+    USE_SERIAL.flush();
+    delay(1000);
+  }
+
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP("Nicebots.com", "R2-D2 C-3P0");
+
+  // set the digital pin as output:
+  pinMode(ledPin, OUTPUT);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+    // wait for WiFi connection
+  if ((WiFiMulti.run() == WL_CONNECTED)) {
+
+    t_httpUpdate_return ret = ESPhttpUpdate.update("http://fw.nicebots.com/update.php");
+    //t_httpUpdate_return  ret = ESPhttpUpdate.update("https://server/file.bin", "", "fingerprint");
+
+    switch (ret) {
+      case HTTP_UPDATE_FAILED:
+        USE_SERIAL.printf("HTTP_UPDATE_FAILD Error (%d): %s", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
+        break;
+
+      case HTTP_UPDATE_NO_UPDATES:
+        USE_SERIAL.println("HTTP_UPDATE_NO_UPDATES");
+        break;
+
+      case HTTP_UPDATE_OK:
+        USE_SERIAL.println("HTTP_UPDATE_OK");
+        break;
+    }
+  }
+
+  unsigned long currentMillis = millis();
+  
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState == LOW) {
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
+    }
+
+    // set the LED with the ledState of the variable:
+    digitalWrite(ledPin, ledState);
+  }
 }
