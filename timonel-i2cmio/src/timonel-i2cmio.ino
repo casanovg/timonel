12a288d6fc24d5f3a09bd1d711b436b37aa67b7e
Gustavo Casanova <gustavo.casanova@nicebots.com>
2018-12-13 01:19:55 -0300

    Store strings to flash

--- timonel-i2cmio/src/timonel-i2cmio.ino
+++ timonel-i2cmio/src/timonel-i2cmio.ino
@@ -6,7 +6,7 @@
 // *  Author: Gustavo Casanova                            *
 // *  ..................................................  *
 // *  Firmware Version: 1.2 | MCU: ESP8266                *
-// *  2018-11-27 gustavo.casanova@nicebots.com            *
+// *  2018-12-13 gustavo.casanova@nicebots.com            *
 // ********************************************************
 //
 // Run timonel-i2cmaster on a NodeMCU, ESP-01 or ESP-12 Module
