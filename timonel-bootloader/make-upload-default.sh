c9550c3e9d23a1d59338bf6b9a78192049b7c70e
gcasanova <gcasanova@WKS-03-NBK.local>
2019-05-31 12:36:50 -0300

    Update make-upload-default.sh

--- timonel-bootloader/make-upload-default.sh
+++ timonel-bootloader/make-upload-default.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
-# ----------------------
+# ........................................................
+# File: make-upload-default.sh
+# Project: Timonel - TWI Bootloader
+# ........................................................
 # 2019-04-07
-# ----------------------
+#
+
+
 ./make-timonel.sh tml-t85-small timonel 13 1B80 1 false;
 ./flash-timonel-bootloader.sh timonel 1;
 ./fuse-read.sh;
