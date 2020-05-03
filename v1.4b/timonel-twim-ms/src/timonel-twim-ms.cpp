a97ad4f6a1cbf318d2ac595cde7a1fda4e852a7f
Gustavo Casanova <gustavo.casanova@nicebots.com>
2020-05-03 15:58:06 -0300

    Update .gitignore, get-github-lib.py, and 127 more files...

--- v1.4b/timonel-twim-ms/src/timonel-twim-ms.cpp
+++ v1.4b/timonel-twim-ms/src/timonel-twim-ms.cpp
@@ -23,10 +23,10 @@
    7) Repeats the routine 3 times.
 */
 
-//#include <Arduino.h>
-//#include <Memory>
-#include "NbMicro.h"
-#include "TimonelTwiM.h"
+#include <NbMicro.h>
+#include <TimonelTwiM.h>
+#include <TwiBus.h>
+
 #include "payload.h"
 
 #define USE_SERIAL Serial
@@ -115,7 +115,7 @@ void setup() {
                 if (errors == 0) {
                     USE_SERIAL.printf_P("OK!\n\r");
                 } else {
-                    USE_SERIAL.printf_P("Error!\n\r", errors);
+                    USE_SERIAL.printf_P("Error! (%d)\n\r", errors);
                 }
                 delay(1000);
                 USE_SERIAL.printf_P("\n\rGetting status of device %d\n\r", dev_info_arr[i].addr);
@@ -133,7 +133,7 @@ void setup() {
                 if (errors == 0) {
                     USE_SERIAL.printf_P("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b successful!      \n\r");
                 } else {
-                    USE_SERIAL.printf_P("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b error!           \n\r", errors);
+                    USE_SERIAL.printf_P("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b error! (%d)           \n\r", errors);
                 }
                 delay(10);
                 USE_SERIAL.printf_P("\n\rGetting status of device %d\n\r", dev_info_arr[i].addr);
---
+++ v1.4c/timonel-twim-ms/README.md
@@ -0,0 +1,7 @@
+# Timonel TWI master - Multi slave
+
+This ESP8266/Arduino test application [shows](https://youtu.be/PM9X1thrdOY) the usage of TWI master libraries.
+
+It is a serial console-based application that runs a 3-time loop that flashes, deletes and runs a user application on three Tiny85's (two bare chips and a Digispark).
+
+The application has been tested on ESP-01 and NodeMCU modules. It is compiled and flashed to the device using [PlatformIO](http://platformio.org) over [VS Code](http://code.visualstudio.com).
