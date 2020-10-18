f9f625aec30e11f34515b187fc83fe678783d4b9
Gustavo Casanova <gustavo.casanova@gmail.com>
2020-10-18 03:47:24 -0300

    Add ATtiny2313 config

--- timonel-bootloader-ioel/lib/nb-usitwisl-if/src/hw-mapping.h
+++ timonel-bootloader-ioel/lib/nb-usitwisl-if/src/hw-mapping.h
@@ -49,19 +49,19 @@
 #if defined(__AVR_ATtiny24__) | \
     defined(__AVR_ATtiny44__) | \
     defined(__AVR_ATtiny84__)
-#define DDR_USI DDRA                // I2C data direction register
-#define PORT_USI PORTA              // I2C output register
-#define PIN_USI PINA                // I2C input register
-#define PORT_USI_SDA PORTA6         // I2C SDA output register
-#define PORT_USI_SCL PORTA4         // I2C SCL output register
-#define PIN_USI_SDA PINA6           // I2C SDA input register
-#define PIN_USI_SCL PINA4           // I2C SDL input register
-#define TWI_START_COND_FLAG USISIF  // Status register flag: indicates an I2C START condition on the bus (can trigger an interrupt)
-#define USI_OVERFLOW_FLAG USIOIF    // Status register flag: indicates a complete bit reception/transmission (can trigger an interrupt)
-#define TWI_STOP_COND_FLAG USIPF    // Status register flag: indicates an I2C STOP condition on the bus
-#define TWI_COLLISION_FLAG USIDC    // Status register flag: indicates a data output collision on the bus
-#define TWI_START_COND_INT USISIE   // Control register bit: defines whether an I2C START condition triggers an interrupt
-#define USI_OVERFLOW_INT USIOIE     // Control register bit: defines whether a USI 4-bit counter overflow triggers an interrupt
+#define DDR_USI DDRA
+#define PORT_USI PORTA
+#define PIN_USI PINA
+#define PORT_USI_SDA PORTA6
+#define PORT_USI_SCL PORTA4
+#define PIN_USI_SDA PINA6
+#define PIN_USI_SCL PINA4
+#define TWI_START_COND_FLAG USISIF
+#define USI_OVERFLOW_FLAG USIOIF
+#define TWI_STOP_COND_FLAG USIPF
+#define TWI_COLLISION_FLAG USIDC
+#define TWI_START_COND_INT USISIE
+#define USI_OVERFLOW_INT USIOIE
 #endif
 
 // ATtiny2313
@@ -73,9 +73,15 @@
 #define PORT_USI_SCL PB7
 #define PIN_USI_SDA PINB5
 #define PIN_USI_SCL PINB7
-#define USI_START_COND_INT USISIF
-#define USI_START_VECTOR USI_START_vect
-#define USI_OVERFLOW_VECTOR USI_OVERFLOW_vect
+#define TWI_START_COND_FLAG USISIF
+#define USI_OVERFLOW_FLAG USIOIF
+#define TWI_STOP_COND_FLAG USIPF
+#define TWI_COLLISION_FLAG USIDC
+#define TWI_START_COND_INT USISIE
+#define USI_OVERFLOW_INT USIOIE
+// #define USI_START_COND_INT USISIF
+// #define USI_START_VECTOR USI_START_vect
+// #define USI_OVERFLOW_VECTOR USI_OVERFLOW_vect
 #endif
 
 // ATtiny167
